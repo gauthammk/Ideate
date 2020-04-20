32b02375d85338d6e16e289fabe74478aba46b6d
gauthammk <gr8gautham@gmail.com>
2020-04-20 19:17:01 +0530

    updated settings

--- ideate/settings.py
+++ ideate/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2+qjfv9^6=l$d8e$38!(y+dj)%05ic-)etl8zlp*)ndc3ui(ln'
+# SECRET_KEY = '2+qjfv9^6=l$d8e$38!(y+dj)%05ic-)etl8zlp*)ndc3ui(ln'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
